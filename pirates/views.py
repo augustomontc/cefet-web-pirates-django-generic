96be0a09fcbf76fe8198c733c6490c2057553166
augustomontc <augustocosta2609@gmail.com>
2018-12-02 18:08:00 -0200

    Exercicio 3

--- pirates/views.py
+++ pirates/views.py
@@ -5,22 +5,29 @@ from django.views import View
 from django.forms import ModelForm
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 from .models import Tesouro
 
 # Create your views here.
-class ListarTesouros(View):
-    def get(self,request):
-        lst_tesouros = Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('quantidade')*F('preco'),\
+class ListarTesouros(ListView):
+    model = Tesouro
+    template_name = 'lista_tesouros.html'
+
+    def get_queryset(self):
+        return Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('quantidade')*F('preco'),\
                             output_field=DecimalField(max_digits=10,\
                                                     decimal_places=2,\
                                                      blank=True)\
                                                     )\
                             )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         valor_total = 0
-        for tesouro in lst_tesouros:
+        for tesouro in context.get('object_list',[]):
             valor_total += tesouro.valor_total
-        return render(request,"lista_tesouros.html",{"lista_tesouros":lst_tesouros,
-                                                     "total_geral":valor_total})
+        context.update({'total_geral': valor_total})
+        return context
 
 class RemoverTesouro(DeleteView):
     model = Tesouro
